df004f84e6273a48f694e8fe13739b465d5f28b3
Peter Ajtai <peter.ajtai@thinksolid.com>
2013-07-09 09:10:08 -0700

    updating the template file

--- template.js
+++ template.js
@@ -1,81 +1,85 @@
 /*
- * grunt-init-node
+ * grunt-init-reveal-jade
  * https://gruntjs.com/
  *
- * Copyright (c) 2013 "Cowboy" Ben Alman, contributors
+ * Copyright (c) 2013 Peter Ajtai
  * Licensed under the MIT license.
  */
 
 'use strict';
 
 // Basic template description.
-exports.description = 'Create a Node.js module, including Nodeunit unit tests.';
+exports.description = 'Create a RevealJS slideshow including support for livereload.';
 
 // Template-specific notes to be displayed before question prompts.
-exports.notes = '_Project name_ shouldn\'t contain "node" or "js" and should ' +
-  'be a unique ID not already in use at search.npmjs.org.';
+exports.notes = '';
 
 // Template-specific notes to be displayed after question prompts.
 exports.after = 'You should now install project dependencies with _npm ' +
-  'install_. After that, you may execute project tasks with _grunt_. For ' +
-  'more information about installing and configuring Grunt, please see ' +
-  'the Getting Started guide:' +
-  '\n\n' +
-  'http://gruntjs.com/getting-started';
+    'install_. After that, you may begin editing your slideshow by running ' +
+    '_grunt server_. This will create a ready to use build. Should you want ' +
+    'to create a build without running the server, run _grunt build_. ' +
+    'For more information about installing and configuring Grunt, please see ' +
+    'the Getting Started guide:' +
+    '\n\n' +
+    'http://gruntjs.com/getting-started';
 
 // Any existing file or directory matching this wildcard will cause a warning.
 exports.warnOn = '*';
 
 // The actual init template.
-exports.template = function(grunt, init, done) {
+exports.template = function (grunt, init, done) {
 
-  init.process({type: 'node'}, [
-    // Prompt for these values.
-    init.prompt('name'),
-    init.prompt('description'),
-    init.prompt('version'),
-    init.prompt('repository'),
-    init.prompt('homepage'),
-    init.prompt('bugs'),
-    init.prompt('licenses'),
-    init.prompt('author_name'),
-    init.prompt('author_email'),
-    init.prompt('author_url'),
-    init.prompt('node_version', '>= 0.8.0'),
-    init.prompt('main'),
-    init.prompt('npm_test', 'grunt nodeunit'),
-    {
-      name: 'travis',
-      message: 'Will this project be tested with Travis CI?',
-      default: 'Y/n',
-      warning: 'If selected, you must enable Travis support for this project in https://travis-ci.org/profile'
-    },
-  ], function(err, props) {
-    props.keywords = [];
-    props.devDependencies = {
-      'grunt-contrib-jshint': '~0.6.0',
-      'grunt-contrib-nodeunit': '~0.2.0',
-      'grunt-contrib-watch': '~0.4.0',
-    };
-    // TODO: compute dynamically?
-    props.travis = /y/i.test(props.travis);
-    props.travis_node_version = '0.10';
+    init.process({type : 'reveal-jade'}, [
+        // Prompt for these values.
+        init.prompt('name'),
+        init.prompt('description'),
+        init.prompt('version'),
+        init.prompt('repository'),
+        init.prompt('homepage'),
+        init.prompt('bugs'),
+        init.prompt('licenses'),
+        init.prompt('author_name'),
+        init.prompt('author_email'),
+        init.prompt('author_url'),
+        init.prompt('node_version', '>= 0.8.19'),
+        init.prompt('slidesDirectory'),
+        init.prompt('buildDirectory'),
+        init.prompt('assetsDirectory'),
+        init.prompt('cleanBeforeBuild'),
+        init.prompt('slideshowTitle'),
+        init.prompt('theme'),
+        init.prompt('syntaxHlTheme'),
+        init.prompt('controls'),
+        init.prompt('progress'),
+        init.prompt('history'),
+        init.prompt('center'),
+        init.prompt('transition')
+    ], function (err, props) {
+        props.keywords = [];
+        props.devDependencies = {
+            "matchdep"                 : "~0.1.2",
+            "grunt-reveal-jade"        : "git://github.com/pajtai/grunt-reveal-jade.git#master",
+            "grunt-open"               : "~0.2.0",
+            "grunt-contrib-watch"      : "~0.4.4",
+            "grunt-contrib-connect"    : "~0.3.0",
+            "grunt-contrib-livereload" : "git://github.com/pajtai/grunt-contrib-livereload.git#multi-write"
+        };
 
-    // Files to copy (and process).
-    var files = init.filesToCopy(props);
-    if (!props.travis) { delete files['.travis.yml']; }
+        // Files to copy (and process).
+        var files = init.filesToCopy(props);
 
-    // Add properly-named license files.
-    init.addLicenseFiles(files, props.licenses);
+        // Add properly-named license files.
+        init.addLicenseFiles(files, props.licenses);
 
-    // Actually copy (and process) files.
-    init.copyAndProcess(files, props);
+        // Actually copy (and process) files.
+        init.copyAndProcess(files, props);
 
-    // Generate package.json file.
-    init.writePackageJSON('package.json', props);
+        // Generate package.json file.
+        init.writePackageJSON('package.json', props);
 
-    // All done!
-    done();
-  });
+        // All done!
+        done();
+    });
 
 };
