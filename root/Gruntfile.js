ca510822fd302203a3b893a71ad9c5a732935423
Peter Ajtai <peter.ajtai@thinksolid.com>
2013-07-09 19:33:44 -0700

    adding templating variables

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -1,48 +1,81 @@
-'use strict';
-
+/*global module:false, require:false*/
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    nodeunit: {
-      files: ['test/**/*_test.js'],
-    },
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib: {
-        src: ['lib/**/*.js']
-      },
-      test: {
-        src: ['test/**/*.js']
-      },
-    },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib: {
-        files: '<%= jshint.lib.src %>',
-        tasks: ['jshint:lib', 'nodeunit']
-      },
-      test: {
-        files: '<%= jshint.test.src %>',
-        tasks: ['jshint:test', 'nodeunit']
-      },
-    },
-  });
+    "use strict";
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    var path = require('path'),
+        lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet,
+        folderMount = function folderMount(connect, point) {
+            return connect.static(path.resolve(point));
+        };
 
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'nodeunit']);
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
+    // Project configuration.
+    grunt.initConfig({
+
+        reveal: {
+
+            livereload: {
+                options: {
+                    slides: {%= slidesDirectory %},
+                    build: {%= buildDirectory %},
+                    temp: {%= tempDirectory %},
+                    assets: {%= assetsDirectory %},
+                    cleanBuild: {%= cleanBeforeBuild %},
+                    title: {%= slideshowTitle %},
+                    description: {%= slideshowDescription %},
+                    author: {%= slideshowAuthor %},
+                    theme: {%= theme %},
+                    syntax: {%= syntaxHlTheme %},
+                    controls: {%= controls %},
+                    progress: {%= progress %},
+                    history: {%= history %},
+                    center: {%= center %},
+                    // default/cube/page/concave/zoom/linear/none
+                    transition: {%= transition %}
+                }
+            }
+        },
+
+        watch: {
+            options: {
+                // Start a live reload server on the default port: 35729
+                livereload: true,
+                nospawn: true
+            },
+            jade: {
+                files: ['{%= slidesDirectory %}/*.jade'],
+                tasks: ["reveal-createBuild", "reveal-deleteTemp"]
+            },
+            gruntfile: {
+                files: ['Gruntfile.js'],
+                tasks: ["reveal:livereload", "open"]
+            }
+        },
+
+        connect: {
+            livereload : {
+                options : {
+                    port       : 9001,
+                    hostname: 'localhost',
+                    base       : './build',
+                    middleware : function (connect, options) {
+                        return [lrSnippet, folderMount(connect, options.base)]
+                    }
+                }
+            }
+        },
+
+        open : {
+            reload : {
+                path : 'http://localhost:9001/'
+            }
+        }
+
+    });
+
+    grunt.registerTask("server", "Build and watch task", ["reveal:livereload", "connect", "open", "watch"]);
+    grunt.registerTask("refresh", "Build and watch task", ["reveal:livereload", "open"]);
 };
